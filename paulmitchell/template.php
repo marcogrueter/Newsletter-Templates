b5ffbbf56add9a2992681cdf15ec5e7ae13beca2
Marco Grüter <marco.grueter@healthworld.ch>
2013-01-30 22:14:47 +0100

    let's try that again.. template for paul mitchell

---
+++ paulmitchell/template.php
@@ -0,0 +1,15 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Email_Template_Paulmitchell extends Theme {
+
+	public $author				= 'Marco Grüter - Healthworld (Schweiz) AG';
+	public $author_website		= 'http://www.healthworld.ch/';
+	public $version				= '1.0';
+
+	public $name				= 'Paul Mitchell';
+	public $website				= 'http://www.paulmitchell.ch';
+	public $description			= 'Newsletter template for Paul Mitchell Schweiz';
+
+	public $category			= 'premium';
+
+}/* End of file template.php */
